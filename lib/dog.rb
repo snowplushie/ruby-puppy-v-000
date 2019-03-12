09390c71931cea2108f2a53bed2d4e1828c975e7
Paige Wiener <snowplushie@yahoo.com>
2019-03-12 03:48:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    puts @@all.map {|dog| dog.name}
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+end
